0fe084f3089d5a64349e764e68fa968a89ac0fc7
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2023-04-23 16:25:07 +0200

    add group expense type

--- budgetly/types/collection.ts
+++ budgetly/types/collection.ts
@@ -2,3 +2,10 @@ import { Database } from "../lib/database.types";
 
 export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
 export type Expense = Database["public"]["Tables"]["expenses"]["Row"];
+
+//groupexpense is a view
+export type GroupExpense = {
+  group_category: string;
+  sum_quantity: number;
+  sum_price: number;
+};
