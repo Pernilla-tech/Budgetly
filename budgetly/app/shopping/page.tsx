cbb6afa52512884f4de3dec6ad7cca65c63ea805
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2023-04-23 16:32:14 +0200

    add shopping page

--- budgetly/app/shopping/page.tsx
+++ budgetly/app/shopping/page.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import Products from "./products/page";
+import { useRouter } from "next/navigation";
+import { SearchInput } from "./components/searchinput";
+import supabase from "@/components/lib/supabase-client";
+
+import { useAuth } from "@/components/components/providers/supabase-auth-provider";
+import { Expense } from "@/components/types/collection";
 
 const Shopping = () => {
+  //get expenses from supabase
+
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    expensesData();
+  }, [user]);
+
+  const expensesData = async () => {
+    const { data: expenseData, error } = await supabase
+      .from("expenses")
+      .select("*")
+      .ilike("category", "food/%")
+      .eq("profile_id", user?.id);
+    if (error) console.log("error", error);
+    else {
+      setExpenses(expenseData);
+    }
+  };
+
+  console.log("expenses", expensesData);
+  console.log("expensesShopping", expenses);
+
+  const router = useRouter();
   return (
     <div className={styles.main}>
-      Shopping
-      <Products />
+      <h1>Shopping</h1>
+      <div>
+        <button onClick={() => router.push("/shopping")}>shopping</button>
+        <button onClick={() => router.push("/shopping/categories")}>
+          categories
+        </button>
+        <div>
+          <button onClick={() => router.push("shopping/addproducts")}>
+            Add products
+          </button>
+        </div>
+      </div>
+      <SearchInput expenses={expenses} />
     </div>
   );
 };
