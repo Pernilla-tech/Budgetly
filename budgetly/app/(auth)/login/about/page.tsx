dcd7a72db3f78339318ef3c08ec11fc59180ac31
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2023-05-12 15:53:06 +0200

    remove dead code and add minor styling

--- budgetly/app/(auth)/login/about/page.tsx
+++ budgetly/app/(auth)/login/about/page.tsx
@@ -14,14 +14,14 @@ export const About = () => {
           <p>
             Budgetly is a user-friendly and intuitive budget app that helps you
             to manage your finances and expenses in a simple way.
-            <p>
-              With Budgetly you can record your budget for each month and
-              compare your expenses between months. You can also save your
-              expenses in different categories to get an overview of how much
-              you spend on different areas. Our app gives you an overview of
-              your finances and helps you keep track of your expenses so you can
-              save money and achieve your financial goals.
-            </p>
+          </p>
+          <p>
+            With Budgetly you can record your budget for each month and compare
+            your expenses between months. You can also save your expenses in
+            different categories to get an overview of how much you spend on
+            different areas. Our app gives you an overview of your finances and
+            helps you keep track of your expenses so you can save money and
+            achieve your financial goals.
           </p>
         </div>
       </div>
--- budgetly/app/(auth)/login/components/loginform/loginForm.tsx
+++ budgetly/app/(auth)/login/components/loginform/loginForm.tsx
@@ -18,38 +18,36 @@ const LoginForm = () => {
   }, [router, user]);
 
   return (
-    <main>
-      <div className={styles.container}>
-        <div className={styles.info}>
-          <div className={styles.logo}>
-            <LoginpageSvg />
-          </div>
-          <div className={styles.description}>
-            <h1>Welcome to Budgetly</h1>
-            <p>
-              Budgetly is a user-friendly and intuitive budget app that helps
-              you to manage your finances and expenses in a simple way.
-            </p>
-
-            <p>Budgetly will help you take control over your finances.</p>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <LoginpageSvg />
         </div>
+        <div className={styles.description}>
+          <h1>Welcome to Budgetly</h1>
+          <p>
+            Budgetly is a user-friendly and intuitive budget app that helps you
+            to manage your finances and expenses in a simple way.
+          </p>
 
-        <div className={styles.login}>
-          <span className={styles.description}>
-            <h1>User login</h1>
-          </span>
-
-          <CustomButton
-            className={styles.loginbutton}
-            text="Login with Google"
-            onClick={() => signInWithGoogle()}
-            variant="contained"
-            size="large"
-          />
+          <p>Budgetly will help you take control over your finances.</p>
         </div>
       </div>
-    </main>
+
+      <div className={styles.login}>
+        <span className={styles.description}>
+          <h1>User login</h1>
+        </span>
+
+        <CustomButton
+          className={styles.loginbutton}
+          text="Login with Google"
+          onClick={() => signInWithGoogle()}
+          variant="contained"
+          size="large"
+        />
+      </div>
+    </div>
   );
 };
 
