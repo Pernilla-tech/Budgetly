d66bbaf29ccb67c9ca44b65972be548ff3233680
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2023-05-19 10:06:15 +0200

    format month to a string

--- budgetly/app/page.tsx
+++ budgetly/app/page.tsx
@@ -8,8 +8,9 @@ const Home = () => {
 
   useEffect(() => {
     const month = new Date().getMonth() + 1;
+    const formattedMonth = month.toString().padStart(2, "0");
     const year = new Date().getFullYear();
-    router.replace(`/${year}/${month}/overview`);
+    router.replace(`/${year}/${formattedMonth}/overview`);
   }, [router]);
 };
 
