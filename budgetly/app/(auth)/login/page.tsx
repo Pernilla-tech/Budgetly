f980434299b40979817af0feaf7a446150d6f969
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2023-07-02 15:43:26 +0200

    Remove metadata from client-side page

--- budgetly/app/(auth)/login/page.tsx
+++ budgetly/app/(auth)/login/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { Metadata } from "next";
 import LoginForm from "./components/loginform/loginForm";
 import styles from "./page.module.css";
 
 import SparklesComponent from "./components/sparkles/sparkles";
 import { Overlay } from "./components/overlay/overlay";
-
-export const metadata: Metadata = {
-  title: "Log in",
-  description: "Log in to Budgetly",
-};
 
 const LoginPage = () => {
   return (
